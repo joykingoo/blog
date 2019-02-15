9278f2c4bfbc54403adf4f5c9591d5070c1fc21c
joy <wendokingoo@gmail.com>
2019-02-15 17:10:33 +0300

    config file modified

---
+++ config.py
@@ -0,0 +1,47 @@
+import os
+
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    
+    # SECRET_KEY = os.environ.get('SECRET_KEY')
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://joy:joy@localhost/pitch'
+
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'Pitch'
+    SENDER_EMAIL = 'wendokingoo@gmail.com'
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("HEROKU_POSTGRESQL_NAVY_URL")
+    pass
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig,
+    
+}
