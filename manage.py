c24de1acd57ac1b2dcceb89fb342e2ebf81ffd59
joy <wendokingoo@gmail.com>
2019-02-19 12:09:48 +0300

    deployment to heroku

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from app.models import User,Posts,PhotoProfile
 from  flask_migrate import Migrate, MigrateCommand
 
 #creating app
-app = create_app("development")
+app = create_app("production")
 
 
 manager = Manager(app)
